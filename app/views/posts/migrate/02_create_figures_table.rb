512efd41dcdcf3436989f0ebba76ea8da6006c2f
askoenig <askoenig94@gmail.com>
2019-05-18 17:51:16 -0400

    Done.

---
+++ app/views/posts/migrate/02_create_figures_table.rb
@@ -0,0 +1,9 @@
+class CreateFiguresTable < ActiveRecord::Migration
+  # raise 'Write CreateLandmarks migration here'
+  def change
+  create_table :figures do |t|
+    t.string :name
+
+    end
+  end
+end
